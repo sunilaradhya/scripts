9fa7d30962e3bb5d36da0fa278ab4550e4594418
sunilaradhya <sunilms974@gmail.com>
2022-07-27 12:31:14 +0000

    adding scripts

---
+++ bubblesort.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+arr=(10 8 20 100 12)
+
+echo "Array in original order"
+echo ${arr[*]}
+
+for ((i = 0; i<5; i++))
+do
+
+    for((j = 0; j<5-i-1; j++))
+    do
+
+        if [ ${arr[j]} -gt ${arr[$((j+1))]} ]
+        then
+            
+            temp=${arr[j]}
+            arr[$j]=${arr[$((j+1))]}
+            arr[$((j+1))]=$temp
+        fi
+    done
+done
+
+echo "Array in sorted order :"
+echo ${arr[*]}
